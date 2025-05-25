b584a14530909a3aed123549e66753894529e450
dimonkiss <dklxwa@gmail.com>
2025-05-25 18:55:00 +0300

    Refactor map

--- pacman.py
+++ pacman.py
@@ -14,6 +14,7 @@ class Pacman(object):
         self.node = node
         self.setPosition()
         self.target = node
+        self.collideRadius = 5
 
     def setPosition(self):
         self.position = self.node.position.copy()
@@ -23,6 +24,8 @@ class Pacman(object):
         direction = self.getValidKey()
         if self.overshotTarget():
             self.node = self.target
+            if self.node.neighbors[PORTAL] is not None:
+                self.node = self.node.neighbors[PORTAL]
             self.target = self.getNewTarget(direction)
             if self.target is not self.node:
                 self.direction = direction
@@ -78,6 +81,15 @@ class Pacman(object):
             if direction == self.direction * -1:
                 return True
         return False
+    
+    def eatPellets(self, pelletList):
+        for pellet in pelletList:
+            d = self.position - pellet.position
+            dSquared = d.magnitudeSquared()
+            rSquared = (pellet.radius+self.collideRadius)**2
+            if dSquared <= rSquared:
+                return pellet
+        return None
 
     def render(self, screen):
         p = self.position.asInt()
