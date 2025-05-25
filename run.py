cfd8d87e4b6166772d53821e0d1a37698cfa912b
dimonkiss <dklxwa@gmail.com>
2025-05-25 18:47:46 +0300

    Add basic Movement

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 import pygame
 from pygame.locals import *
 from constants import *
+from pacman import Pacman
 
 class GameController(object):
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode(SCREENSIZE, 0, 32)
         self.background = None
+        self.clock = pygame.time.Clock()
 
     def setBackground(self):
         self.background = pygame.surface.Surface(SCREENSIZE).convert()
@@ -14,8 +16,11 @@ class GameController(object):
 
     def startGame(self):
         self.setBackground()
+        self.pacman = Pacman()
 
     def update(self):
+        dt = self.clock.tick(30) / 1000.0
+        self.pacman.update(dt)
         self.checkEvents()
         self.render()
 
@@ -25,6 +30,8 @@ class GameController(object):
                 exit()
 
     def render(self):
+        self.screen.blit(self.background, (0, 0))
+        self.pacman.render(self.screen)
         pygame.display.update()
 
     
